2e470ab4b569677dfcc9d90bb8a393ff15bba121
Abhinam11 <abhinamkhatiyan806@gmail.com>
2024-08-01 00:09:09 +0530

    API for listing all teachers for principal

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -4,7 +4,7 @@ from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment , Teacher
 
-from .schema import AssignmentSchema, AssignmentGradeSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema , TeacherSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 @principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
@@ -14,3 +14,11 @@ def list_assignments(p):
     principal_assignments = Assignment.get_assignment_by_principal()
     principal_assignments_dump = AssignmentSchema().dump(principal_assignments, many=True)
     return APIResponse.respond(data=principal_assignments_dump)
+
+@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+     """Returns list of assignments"""
+     list_of_teachers = Teacher.get_teachers_by_principal()
+     list_of_teachers_dump = TeacherSchema().dump(list_of_teachers,many = True)
+     return APIResponse.respond(data=list_of_teachers_dump)
